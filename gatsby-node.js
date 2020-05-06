4f081783bbd7898a3acdf8f5d362d91a8e456c18
yaldram <arsalan.yaldram@hybrowlabs.com>
2020-05-06 19:32:18 +0530

    Created Gatsby Front Pages.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,38 @@
-// exports.createPages = async function ({ actions, graphql }) {
-//   const { data } = await graphql`
-//     query {
-//       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-//         edges {
-//           node {
-//             id
-//             frontmatter {
-//                 slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `;
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            id
+            frontmatter {
+                slug
+            }
+          }
+        }
+      }
+    }
+  `);
 
-//   // Create Paginated Posts
-//   const postPerPage = 3;
+  // Create Paginated Posts
+  const postPerPage = 3;
 
-//   const numPages = Math.ceil(data.allMdx.edges.length / 3);
+  const numPages = Math.ceil(data.allMdx.edges.length / 3);
 
-//   Array.from({ length: numPages }).forEach((_, index) => {
-//     actions.createPages({
-//       path: i === 0 ? `/` : `/${i + 1}`,
-//       component: require.resolve("./src/templates/allPosts.js"),
-//       context: {
-//         limit: postPerPage,
-//         skip: index * postPerPage,
-//         numPages,
-//         currentPage: index + 1
-//       }
-//     })
-//   });
+  Array.from({ length: numPages }).forEach((_, index) => {
+    actions.createPage({
+      path: index === 0 ? `/` : `/${i + 1}`,
+      component: require.resolve("./src/templates/allPosts.js"),
+      context: {
+        limit: postPerPage,
+        skip: index * postPerPage,
+        numPages,
+        currentPage: index + 1
+      }
+    })
+  });
 
-//   // Create Single Blog Post
+  // Create Single Blog Post
 //   data.allMdx.edges.forEach(edge => {
 //     const slug = edge.node.frontmatter.slug;
 //     const id = edge.node.id;
@@ -43,4 +43,4 @@
 //     })
 //   })
 
-// }
+}
---
+++ src/templates/allPosts.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { graphql } from "gatsby"
+
+import { Container, Content, Card, FeatureImage, Pagination } from "../components"
+import { H1, P } from "../elements";
+
+const allPosts = ({ pageContext, data }) => {
+    const { currentPage, numPages } = pageContext;
+    const isFirst = currentPage === 1;
+    const isLast = currentPage === numPages;
+
+    const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`;
+    const nextPage = `/${currentPage} + 1`;
+
+    const posts = data.allMdx.edges;
+
+    return (
+      <Container>
+        <FeatureImage />
+        <Content>
+          <H1 textAlign="center" margin="0 0 1rem 0">
+            This is a Dummy title.
+          </H1>
+          <P textAlign="center" color="dark2">
+            This is some Dummy Content.
+          </P>
+          {posts.map(post => (
+            <Card
+              key={post.node.frontmatter.slug}
+              date={post.node.frontmatter.date}
+              title={post.node.frontmatter.title}
+              excerpt={post.node.frontmatter.excerpt}
+              slug={post.node.frontmatter.slug}
+            />
+          ))}
+        </Content>
+        <Pagination
+          isFirst={isFirst}
+          isLast={isLast}
+          prevPage={prevPage}
+          nextPage={nextPage}
+        />
+      </Container>
+    )
+
+}
+
+export default allPosts;
+
+export const pageQuery = graphql `
+  query AllPostsQuery($skip: Int!, $limit: Int!) {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}, skip: $skip, limit: $limit) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            date(formatString: "MMMM DD, YYYY")
+            excerpt
+            title
+          }
+        }
+      }
+    }
+  }
+`;
